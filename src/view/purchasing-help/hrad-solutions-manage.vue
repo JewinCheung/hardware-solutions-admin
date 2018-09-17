878af026cef9515a16e20a0a6fc3235a0d6872d0
Jewin <zjw240@qq.com>
2018-09-17 23:40:25 +0800

    方案分配

--- src/view/purchasing-help/hrad-solutions-manage.vue
+++ src/view/purchasing-help/hrad-solutions-manage.vue
@@ -1,74 +1,372 @@
 <template>
-
   <div class="hrad-solutions-manage">
 
     <Row :gutter="20">
-      <Col span="16">
+      <Col>
       <Card>
+        <Row :gutter="10" style="margin-bottom: 10px;">
+          <Col span="16">
+          <Card>
+            <div style="margin-bottom: 10px;">
+              产品线：
+              <Select style="width:160px" v-model="lineName" @on-change="proChange">
+                <Option v-for="item in ProLine" :value="item" :key="item">{{ item }}</Option>
+              </Select>
+              <Button class="search-btn" type="primary" @click="setSmall">
+                <Icon type="ios-download-outline" size="16" />&nbsp;&nbsp;保存方案</Button>
+            </div>
+            <Transfer :data="hardList" :target-keys="targetKeys" :render-format="renderTarget" @on-change="handleChange"
+              :list-style="listStyle" filterable></Transfer>
+          </Card>
+          </Col>
+          <Col span="8" class="padding-left-10">
+          <Card>
+            <p slot="title">
+              <Icon type="ios-paper-outline"></Icon>
+              分配方案-[{{lineName}}]
+            </p>
+            <div style="height: 325px;">
+              <ul class="iview-admin-draggable-list">
+                <li v-for="(item, index) in smallType" :key="item.key" class="notwrap todolist-item" :data-index="index"
+                  @click="smallTypeClick(item)">
+                  {{item.label}}
+                </li>
+              </ul>
+            </div>
+          </Card>
+          </Col>
+        </Row>
 
-        <Transfer :data="hardList" :target-keys="targetKeys4" :render-format="render4" @on-change="handleChange4"
-          :list-style="listStyle" filterable></Transfer>
+        <Row :gutter="10" >
+
+            <Col>
+                  <div class="tools">
+        <ButtonGroup>
+          <Button type="primary">保存</Button>
+        </ButtonGroup>
+      </div>
+            <Table :data="hardItmeList" :columns="ItmeColumns" stripe ref="tables"></Table>
+            <div style="margin: 10px;overflow: hidden">
+              <div style="float: right;">
+                <Page :total="1" :current="1" @on-change="changePage"></Page>
+              </div>
+            </div>
+            </Col>
+
+        </Row>
+
       </Card>
       </Col>
     </Row>
   </div>
 </template>
+
 <script>
-  import {
-    getProLine,
-    getHardType,
-    getHardList
-  } from '@/api/data'
-  export default {
-    name: 'hrad-solutions-manage',
-    data() {
-      return {
-        hardList: this.getMockData(),
-        targetKeys4: this.getTargetKeys(),
-        listStyle: {
-          width: '250px',
-          height: '300px'
-        }
+import { getProLine, getHardType, getHardList } from '@/api/data'
+export default {
+  name: 'hrad-solutions-manage',
+  data () {
+    return {
+      hardList: [],
+      targetKeys: [],
+      ProLine: [], // 产品线
+      smallType: [],
+      lineName: '未选择',
+      hardItmeList: [],
+      ItmeColumns: [],
+      listStyle: {
+        width: '45%',
+        height: '334px'
       }
+    }
+  },
+  mounted () {
+    // 硬件列表
+    this.getMockData()
+    // 初始化产品线
+    getProLine().then(res => {
+      this.ProLine = res.data
+    })
+    this.getHardItemData()
+    this.sethardColumns()
+  },
+  methods: {
+    proChange (Option) {
+      this.lineName = Option
+      this.targetKeys = []
     },
-    mounted() {
-      // 硬件列表
-      getHardList().then(res => {
+    getMockData () {
+      getHardType().then(res => {
         var hardList = res.data
+        var typeData = []
+        hardList.map(item => {
+          var bigType = item.bigType
 
-        this.hardList = hardList.map(item => {
-
-
+          item.smallType.map((i, index) => {
+            return typeData.push({
+              key: typeData.length,
+              label: i,
+              type: bigType
+            })
+          })
         })
-
+        this.hardList = typeData
+        this.getTargetKeys()
+        // console.log(this.hardList);
       })
     },
-    methods: {
-      getMockData() {
-        let mockData = [];
-        for (let i = 1; i <= 20; i++) {
-          mockData.push({
-            key: i.toString(),
-            label: 'Content ' + i,
-            description: 'The desc of content  ' + i,
-          });
-        }
-        return mockData;
-      },
-      getTargetKeys() {
-        return this.getMockData()
-          .filter(() => Math.random() * 2 > 1)
-          .map(item => item.key);
-      },
-      handleChange4(newTargetKeys) {
-        this.targetKeys4 = newTargetKeys;
-      },
-      render4(item) {
-        return item.label + ' - ' + item.description;
+    getTargetKeys () {
+      var hardList = this.hardList
+        .filter((i, inedx) => inedx > 10)
+        .map(item => item.key)
+      this.targetKeys = hardList
+    },
+    handleChange (newTargetKeys) {
+      this.targetKeys = newTargetKeys
+    },
+    renderTarget (item) {
+      return item.label + '-[' + item.type + ']'
+    },
+    setSmall () {
+      // this.smallType = this.hardList.filter(item => (item.key = 10));
+
+      if (this.targetKeys.length === 0) {
+        this.$Message.warning('目的列表无数据！')
+        return
+      }
+      if (this.lineName === '未选择') {
+        this.$Message.warning('请选择产品线！')
+        return
       }
 
+      this.smallType = this.hardList.filter(item => {
+        var isOk = false
+        this.targetKeys.map(i => {
+          if (item.key === i) {
+            return (isOk = true)
+          }
+        })
+        if (isOk) {
+          return item
+        }
+      })
+    },
+    sethardColumns () {
+      this.ItmeColumns = [
+        {
+          type: 'selection',
+          width: 60,
+          align: 'center'
+        },
+        {
+          title: '大类',
+          key: 'bigType'
+        },
+        {
+          title: '子类',
+          key: 'smallType'
+        },
+        {
+          title: '品牌',
+          key: 'hardBrand'
+        },
+        {
+          title: '型号',
+          key: 'hardModel',
+          tooltip: true
+        },
+        {
+          title: '参数',
+          key: 'hardParams',
+          render: (h, params) => {
+            return h(
+              'tooltip',
+              {
+                props: {
+                  placement: 'top'
+                }
+              },
+              [
+                h('Tag', '···'),
+                h(
+                  'div',
+                  {
+                    slot: 'content'
+                  },
+                  [
+                    h(
+                      'ul',
+                      this.hardItmeList[params.index].hardParams.map(item => {
+                        return h(
+                          'li',
+                          {
+                            style: {
+                              textAlign: 'left',
+                              padding: '4px',
+                              'list-style-type': 'none'
+                            }
+                          },
+                          item.dictType + '：' + item.dictName
+                        )
+                      })
+                    )
+                  ]
+                )
+              ]
+            )
+          }
+        },
+        {
+          title: '质保',
+          key: 'hardWarranty',
+          width: 60,
+          align: 'center'
+        },
+        {
+          title: '质保类型',
+          key: 'warrantyType',
+          width: 90,
+          align: 'center'
+        },
+        {
+          title: '延保费用',
+          key: 'warrantyPrice',
+          width: 90,
+          align: 'center'
+        },
+        {
+          title: '图片',
+          key: '',
+          width: 60,
+          align: 'center',
+          render: (h, params) => {
+            return h('tooltip', [
+              h('Icon', {
+                props: {
+                  type:
+                    this.hardItmeList[params.index].hardImg.length > 0
+                      ? 'ios-eye'
+                      : '',
+                  size: '24'
+                },
+                style: {},
+                on: {
+                  click: () => {
+                    this.show(params.index)
+                  }
+                }
+              }),
 
-    }
+              h(
+                'div',
+                {
+                  slot: 'content'
+                },
+                this.hardItmeList[params.index].hardImg.length > 0
+                  ? '点击预览'
+                  : '无图片信息'
+              )
+            ])
+          }
+        },
+        {
+          title: '节能环保',
+          key: 'IsEnergy',
+          width: 90,
+          align: 'center',
+          render: (h, params) => {
+            const row = params.row
+            const text = row.IsEnergy === 1 ? '是' : '否'
+            return h('Tag', text)
+          }
+        },
+        {
+          title: '价格',
+          key: 'hardPrice'
+        },
+        {
+          title: '更新日期',
+          key: 'CreateTime',
+          width: 100
+        }
+      ]
+    },
+    show (index) {
+      if (this.hardItmeList[index].hardImg.length > 0) {
+        this.$Modal.info({
+          scrollable: true,
+          closable: true,
+          render: h => {
+            return h(
+              'div',
+              this.hardItmeList[index].hardImg.map(item => {
+                return h('img', {
+                  attrs: {
+                    src: item
+                  },
+                  style: {
+                    width: '100%'
+                  }
+                })
+              })
+            )
+          }
+        })
+      }
+    },
+    getHardItemData () {
+      // 硬件列表
+      getHardList().then(res => {
+        this.hardItmeList = res.data
+        console.log(this.hardItmeList)
+        // if (this.smallType.length > 0) {
+        // this.hardItmeList = this.hardItmeList.filter(item => {
+        //   var isOk = false;
+        //   this.smallType.map(i => {
+        //     if (item.label.indexOf(i) > -1) {
+        //       return (isOk = true);
+        //     }
+        //   });
+        //   if (isOk) {
+        //     return item;
+        //   }
+        // });
+        // }
+      })
+    },
+    changePage () {}
   }
-
+}
 </script>
+<style lang="less">
+.tools {
+  margin: 0 0 5 0;
+
+  button {
+    margin: 5px;
+  }
+}
+.iview-admin-draggable-list {
+  height: 100%;
+}
+
+.iview-admin-draggable-list li {
+  padding: 9px;
+  border: 1px solid #e7ebee;
+  border-radius: 3px;
+  margin-bottom: 5px;
+  cursor: pointer;
+  position: relative;
+  transition: all 0.2s;
+}
+
+.iview-admin-draggable-list li:hover {
+  color: #87b4ee;
+  border-color: #87b4ee;
+  transition: all 0.2s;
+}
+
+.iview-admin-draggable-list {
+  overflow: auto;
+}
+</style>
